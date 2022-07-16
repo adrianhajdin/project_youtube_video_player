81bb1e6c6a4f95807176e0a2997775bdeeeab108
adrianhajdin <adrianhajdin00@gmail.com>
2022-07-16 09:28:01 +0200

    ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Typography, Box } from '@mui/material';
+import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Logo from '../logo.png'
 
 import { SearchBar } from './';
 
 const Navbar = () =>  (
   <Box sx={{ display: 'flex', justifyContent: 'space-between', p: 2, pt: 1, borderBottom: '1px solid #e3e3e3', position: 'fixed', top: 0, left: 0, background: 'white', width: '100%', zIndex: 100 }}>
-    <Link to='/' style={{ textDecoration: 'none' }}>
-      <Typography sx={{ fontSize: {sm: 20, md: '25px',marginTop:'13px' }, color: 'red', fontWeight: 800 }}>
-        U📺tube
-      </Typography>
+    <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={Logo} alt="logo" height={45} />
     </Link>
     <SearchBar />
   </Box>
